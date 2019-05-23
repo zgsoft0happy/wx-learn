e57f6a62bbd67b798b418f40ddbb356c24c3d465
杨延彬 <zgsoft_happy@126.com>
2019-05-23 17:08:25 +0800

    add id

--- src/main/java/cn/ideacs/business/wx/learn/service/impl/WxUserBaseServiceImpl.java
+++ src/main/java/cn/ideacs/business/wx/learn/service/impl/WxUserBaseServiceImpl.java
@@ -3,6 +3,7 @@ package cn.ideacs.business.wx.learn.service.impl;
 import cn.ideacs.business.wx.learn.dao.WxUserBaseMapper;
 import cn.ideacs.business.wx.learn.entity.dtos.WxUserBaseDTO;
 import cn.ideacs.business.wx.learn.service.WxUserBaseService;
+import cn.ideacs.business.wx.learn.utils.SerialComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,14 @@ import javax.annotation.Resource;
 public class WxUserBaseServiceImpl implements WxUserBaseService {
 
     @Resource
+    private SerialComponent serialComponent;
+
+    @Resource
     private WxUserBaseMapper wxUserBaseMapper;
 
     @Override
     public WxUserBaseDTO insert(WxUserBaseDTO wxUserBaseDTO) {
+        wxUserBaseDTO.setInfoNo(serialComponent.nextId());
         Long id = wxUserBaseMapper.insert(wxUserBaseDTO);
         return wxUserBaseMapper.getById(id);
     }
